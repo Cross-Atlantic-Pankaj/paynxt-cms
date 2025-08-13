738f8a2348eff352b555cffef6b5ae5d14f88576
Harsh6530 <sharmaharsh634@kgpian.iitkgp.ac.in>
2025-08-14 00:51:07 +0530

    blog page fixed

--- src/app/api/blog-page/blog-content/route.js
+++ src/app/api/blog-page/blog-content/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
 import Blog from '@/models/blog-page/blogcontent';
+import TileTemplate from '@/models/TileTemplate';
 import FormData from 'form-data';
 import fetch from 'node-fetch';
 
